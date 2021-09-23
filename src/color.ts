551015381ad72f5239c3e71aa35565f0c1ebb3c0
Joao Victor <santosjoao.dev@gmail.com>
2021-09-23 15:20:31 -0300

    add colors in project

---
+++ src/color.ts
@@ -0,0 +1,27 @@
+interface color {
+  Black: string,
+  Error: string,
+  Grey: string,
+  GreySaturated: string,
+  Primary: string,
+  PrimaryBrightness: string,
+  PrimarySaturated: string,
+  Secundary: string,
+  SecundaryBrightness: string,
+  White: string
+}
+
+const colorBrand: color = {
+  Black: '#04071A',
+  Error: '#FF4040',
+  Grey: '#737580',
+  GreySaturated: '#B8BACC',
+  Primary: '#2542F7',
+  PrimaryBrightness: '#1327AB',
+  PrimarySaturated: '#405AFF',
+  Secundary: '#F7BF0A',
+  SecundaryBrightness: '#E5B109',
+  White: '#EBEDF7'
+}
+
+export default colorBrand
