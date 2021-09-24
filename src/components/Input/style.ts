b1e9f65f2bdddac5de97c34e09e422b81a47a480
Joao Victor <santosjoao.dev@gmail.com>
2021-09-24 15:17:17 -0300

    add input compoent

---
+++ src/components/Input/style.ts
@@ -0,0 +1,33 @@
+import styled, { css } from 'styled-components';
+import colorBrand from 'src/color';
+const { Black, Grey, GreySaturated, ErrorColor } = colorBrand;
+
+export const Container = styled.section`
+  display: flex;
+  flex-direction: column;
+  width: 405px;
+  & > * {font-size: 1.333rem;}
+  
+  & > label {
+    margin-bottom: 8px;
+    color: ${Black};
+  }
+  & > input {
+    border: none;
+    outline: none;
+
+    border-radius: 8px;
+    border: 1px solid ${GreySaturated};
+    
+    padding: 16px 8px;
+
+    &::placeholder {
+      color: ${Grey}
+    }
+  }
+
+  & > span {
+    font-size: 16px;
+    color: ${ErrorColor};
+  }
+`;
