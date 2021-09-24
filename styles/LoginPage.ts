274db42d1a908f02676c5359a863ccda12a1cdc9
Joao Victor <santosjoao.dev@gmail.com>
2021-09-24 15:17:31 -0300

    add style css for login page

---
+++ styles/LoginPage.ts
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: grid;
+  place-content: center;
+  min-height: 100vh;
+`;
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  margin-top: 32px;
+`;
+
+export const FormTitle = styled.h1`
+  text-align: center;
+  margin-top: calc(48px - 32px);
+`;
+
+export const ButtonAndAnchorSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+
+  & > a {
+    text-align: center;
+  }
+`;
