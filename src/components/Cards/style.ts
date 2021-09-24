38acf360f13c00b9f58d7460272ce6ad09ddb545
Joao Victor <santosjoao.dev@gmail.com>
2021-09-24 15:16:24 -0300

    improve card component

--- src/components/Cards/style.ts
+++ src/components/Cards/style.ts
@@ -3,11 +3,11 @@ import styled from 'styled-components';
 import hexToRgb from 'src/utils/hexToRgb';
 const { White, Black } = colorBrand
 
-export const Container = styled.div`
+export const Container = styled.section`
   background: ${White};
   border-radius: 12px;
   box-shadow: 0px 1px 10px rgba(${hexToRgb(Black)}, .27);
 
   display: inline-block;
-  padding: 40px;
+  padding: 32px;
 `;
