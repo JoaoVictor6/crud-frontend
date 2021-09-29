ed5aafd016aacf386a172d46b32f98944c73d0ab
Joao Victor <santosjoao.dev@gmail.com>
2021-09-29 19:28:40 -0300

    add register page

--- pages/register.tsx
+++ pages/register.tsx
@@ -1,8 +1,28 @@
+import React, { ChangeEvent, FormEvent, useState } from 'react';
 import Cards from '@components/Cards';
 import Textarea from '@components/Textarea';
 import UploadButton from '@components/UploadButton';
-import { Container } from '@PagesStyles/RegisterPage';
-import { ChangeEvent, useState } from 'react';
+import Button from '@components/Button';
+import Input, { ErrorField } from '@components/Input';
+import { 
+	Container,
+	FormTitle,
+	InputDiv,
+	ButtonAndAnchorSection, 
+	ProfilePicture,
+	ProfilePictureSection
+} from '@PagesStyles/RegisterPage';
+
+type formInput = {
+	email: string
+	password: string
+	passwordConfirm: string
+	username: string
+	description: {
+		totalChar: number
+		text: string
+	}
+}
 
 function get64base(file:FileList, cb: (base64: string | ArrayBuffer | null) => void){
 	const fileBlob = file[0];
@@ -12,54 +32,152 @@ function get64base(file:FileList, cb: (base64: string | ArrayBuffer | null) => v
 		cb(reader.result);
 	};
 	reader.onerror = (error) => {
-		console.log(`Error: ${error}`);
+		return `Error: ${error}`;
 	};
-}
-type textAreaElProps = {
-  description: string
-  totalChar: number
 }
 
 export default function Register(): JSX.Element{
-	const maxChar = 255;
-	const [textAreaEl, setTextAreaEl] = useState<textAreaElProps>({
-		description: '',
-		totalChar: 0
+	const [formInputs, setFormInputs] = useState<formInput>({
+		description: {
+			text: '',
+			'totalChar': 0
+		},
+		email: '',
+		password: '',
+		passwordConfirm: '',
+		username: ''
 	});
+	const [emailFieldIsValid, setEmailFieldIsValid] = useState({} as ErrorField);
+	const [passwordFieldIsValid, setPasswordFieldIsValid] = useState({} as ErrorField);
+	const maxChar = 255;
 
-	function inputHandler(event: ChangeEvent<HTMLInputElement>){
-		if(event.target.type === 'file'){
-			const fileValue = event.target.files as FileList;
-			get64base(fileValue, (base64) => {
-				console.log(base64);
+	function submitFormHandler(event:FormEvent) {
+		const { email, password } = formInputs;
+		const minimunPasswordLength = 8;
+		event.preventDefault();
+		if(!email.length){
+			setEmailFieldIsValid({ isInvalid: true, messageError: 'Empty field' });
+		}
+		if(password.length < minimunPasswordLength) {
+			setPasswordFieldIsValid({
+				isInvalid: true, 
+				messageError: 'password must be at least 8 characterspassword must be at least 8 characters'
 			});
 			return;
 		}
+		setPasswordFieldIsValid({ 
+			...passwordFieldIsValid,
+			isInvalid: false
+		});
+		setEmailFieldIsValid({ 
+			...emailFieldIsValid, 
+			isInvalid: false
+		});
 	}
-	function textAreaHandler(event: ChangeEvent<HTMLTextAreaElement>){
-		if(textAreaEl.totalChar === maxChar+1){
+
+	function inputHandler(event: ChangeEvent<HTMLInputElement>): void{
+		if(event.target.type === 'file'){
+			const fileValue = event.target.files as FileList;
+			get64base(fileValue, (base64) => {
+				return base64;
+			});
+			return;
+		}
+		if(event.target.type === 'textarea'){
+			return;
+		}
+
+		setFormInputs({
+			...formInputs,
+			[event.target.name]: event.target.value
+		});
+	}
+	function textAreaHandler(event: ChangeEvent<HTMLTextAreaElement>): void{
+		if(formInputs.description.totalChar === maxChar+1){
 			return;
 		} else {
-			setTextAreaEl({
-				description: event.target.value,
-				totalChar: event.target.value.length
+			setFormInputs({
+				...formInputs,
+				description: {
+					text: event.target.value,
+					totalChar: event.target.value.length
+				}
 			});
 		}
 	}
 	return(
 		<Container>
 			<Cards>
-				<Textarea 
-					idForLabel="description" 
-					labelText="Description"
-					totalChar={textAreaEl.totalChar}
-					maxChar={maxChar}
-					onChange={textAreaHandler}
-				/>
-				<UploadButton 
-					label="Upload image" 
-					onChangeEvent={inputHandler}
-				/>
+				<FormTitle className="title-section">Register</FormTitle>
+				<form onSubmit={submitFormHandler}>
+					<InputDiv>
+						<div>
+							<ProfilePictureSection>
+								<ProfilePicture />
+								<UploadButton 
+									label="Upload image" 
+									onChangeEvent={inputHandler}
+								/>
+							</ProfilePictureSection>
+
+							<Input
+								name="username"
+								idForInput="username" 
+								type="text" 
+								placeholder="Digite seu nome" 
+								labelText="Username"
+								value={formInputs.username}
+								onChange={inputHandler}
+								ErrorField={emailFieldIsValid}
+							/>
+
+							<Input
+								name="password"
+								idForInput="password" 
+								type="password" 
+								placeholder="Digite sua senha" 
+								labelText="Senha"
+								value={formInputs.password}
+								onChange={inputHandler}
+								ErrorField={passwordFieldIsValid}
+							/>
+						</div>
+						<div>
+							<Textarea 
+								idForLabel="description" 
+								labelText="Description"
+								totalChar={formInputs.description.totalChar}
+								maxChar={maxChar}
+								onChange={textAreaHandler}
+							/>
+
+							<Input
+								name="email"
+								idForInput="email" 
+								type="email"
+								placeholder="Digite seu email" 
+								labelText="Email"
+								value={formInputs.email}
+								onChange={inputHandler}
+								ErrorField={emailFieldIsValid}
+							/>
+
+							<Input
+								name="passwordConfirm"
+								idForInput="password-confirm" 
+								type="password"
+								placeholder="Digite sua senha novamente" 
+								labelText="Confirme sua senha"
+								value={formInputs.passwordConfirm}
+								onChange={inputHandler}
+								ErrorField={passwordFieldIsValid}
+							/>
+						</div>
+					</InputDiv>
+					<ButtonAndAnchorSection>
+						<Button fullWidth placeholder="Cadastrar-se"/>
+					</ButtonAndAnchorSection>
+				</form>
 			</Cards>
 		</Container>
 	);
