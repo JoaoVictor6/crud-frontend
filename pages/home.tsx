b6895f3187b5ff8f883d889a9a3b8d57cdd0aaba
Joao Victor dos Santos <santosjoao.dev@gmail.com>
2021-10-16 22:43:35 -0300

    initial setup home page

---
+++ pages/home.tsx
@@ -0,0 +1,14 @@
+import { Container } from '@PagesStyles/HomePage';
+import Head from 'next/head';
+export default function Home(): JSX.Element{
+	return (
+		<>
+			<Head>
+				<title>Home - Crud</title>
+			</Head>
+			<Container>
+        teste
+			</Container>
+		</>
+	);
+}
